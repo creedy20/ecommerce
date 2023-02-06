05594b4e3db6571db34fe8541fe4dbe56c84bd4c
Carolina Reedy <alonmeree@gmail.comgit config --global user.email alonmeree@gmail.com>
2023-02-06 12:11:59 -0700

    creating API

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import next from "next"
+import type { InferGetStaticPropsType} from "next"
+import getAllProducts from "@/framework/shoppify/product/get-all-products"
+
+
+export async function getStaticProps(){
+  const products = await getAllProducts()
+
+  return {
+    props: {
+      products
+    }, 
+    revalidate: 4 * 60 * 60
+  }
+}
+
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+
+  return(
+    <div>
+      {JSON.stringify( products )}
+    </div>
+  )
+}
